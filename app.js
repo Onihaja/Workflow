e4e9416c146a69fc77db7274057d8c47389e4300
Onihaja <ronihajafitahiana@gmail.com>
2026-05-05 11:06:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = window.SUPABASE_URL || 'https://VOTRE_URL.supabase.co'
 const SUPABASE_KEY = window.SUPABASE_KEY || 'VOTRE_ANON_KEY'
 =======
 const SUPABASE_URL = 'https://rvjjdehrbbxlndaydscb.supabase.co'
-const SUPABASE_KEY = '...'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2ampkZWhyYmJ4bG5kYXlkc2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NzI5NDMsImV4cCI6MjA5MzQ0ODk0M30.47jN0OxtNqMdQBxei8yajtDrd_KrBfwaf2SNaFMJFX8'
  main
 
 const { createClient } = supabase
